b8b92e6b2f28d7d9b3b013e414249762968011ac
ajaymittur28 <ajay.cs18@bmsce.ac.in>
2019-08-18 16:09:09 +0530

    Split audio file to individual artists and made travis into a chipmunk

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <SDL.h>
 
-#define AUDIO_FILE "assets/Uber_Everywhere.wav"
+#define AUDIO_COMPLETE "assets/Uber_Everywhere.wav"
+#define AUDIO_TRAVIS "assets/Uber_Everywhere_Travis.wav"
+#define AUDIO_MADEINTYO "assets/Uber_Everywhere_MadeinTYO.wav"
 
 using namespace std;
 
@@ -10,15 +12,34 @@ struct AudioData {
 	Uint32 length;
 };
 
+Uint8* ModifySamples(float pitch, Uint8 *stream, Uint8 *startPos, int streamLength) {
+	//float *stream = (float*)stream;
+	Uint32 length = (Uint32)streamLength;
+	Sint16 *samples = (Sint16*)startPos;
+	float sampleIndex = 0;
+
+	int volume = 100;
+	float factor = volume * 1.0f / 32768.0f;
+	
+	for (Uint32 i = 0; i < length; i++) {
+		stream[i] = samples[(size_t)sampleIndex] * factor;
+		sampleIndex += pitch;
+	}
+
+	return stream;
+}
+
 void AudioCallback(void *userdata, Uint8 *stream, int streamLength) {
-	AudioData *audio = (AudioData *)userdata;
+	AudioData *audio = (AudioData*)userdata;
 	
 	if (audio->length == 0) return;
 
 	Uint32 length = (Uint32)streamLength;
 	length = length > audio->length ? audio->length : length;
 
-	SDL_memcpy(stream, audio->pos, length);
+	Uint8 *modifiedSamplesPos = ModifySamples(1.0f, stream, audio->pos, length);
+
+	//SDL_memcpy(stream, audio->pos, length);
 	audio->pos += length;
 	audio->length -= length;
 }
@@ -30,10 +51,12 @@ int main(int argc, char *argv[]) {
 	Uint8 *wavStart;
 	Uint32 wavLength;
 
-	if (SDL_LoadWAV(AUDIO_FILE, &wavSpec, &wavStart, &wavLength) == NULL) {
+	if (SDL_LoadWAV(AUDIO_TRAVIS, &wavSpec, &wavStart, &wavLength) == NULL) {
 		cerr << SDL_GetError() << endl;
 		return -1;
 	}
+
+	//wavLength = ModifySamples(, wavStart, wavLength, 2.0);
 
 	AudioData audio = { wavStart, wavLength };
 	wavSpec.callback = AudioCallback;
@@ -47,7 +70,8 @@ int main(int argc, char *argv[]) {
 	else {
 		cout << "Frequency: " << wavSpec.freq << endl
 			 << "Format: " << wavSpec.format << endl
-			 << "Samples: " << wavSpec.samples << endl;
+			 << "Samples: " << wavSpec.samples << endl
+			 << "-------------------------------------" << endl;
 	}
 
 	SDL_PauseAudioDevice(deviceID, 0);
